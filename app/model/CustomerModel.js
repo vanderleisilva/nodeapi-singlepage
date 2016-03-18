573c708c376b1914487302498d98926f85d0bc12
Vanderlei Silva <vanderleisilva@santos.sp.gov.br>
2016-03-18 11:26:36 -0300

    Consider name as a required field

--- app/model/CustomerModel.js
+++ app/model/CustomerModel.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var CustomerSchema = new mongoose.Schema({
 	id: Number,
-  	name: String,
-  	category: String
+	name: { type:String, required:true },
+	category: { type:String }
 });
 
 module.exports = mongoose.model('Customer', CustomerSchema);
--- app/route/CustomerRoute.js
+++ app/route/CustomerRoute.js
@@ -5,50 +5,35 @@ var Customer = require('../model/CustomerModel.js');
 /* GET /customers listing. */
 router.get('/', function(req, res, next) {
 	Customer.find(function (err, data) {
-		if (err){
-			return next(err);
-		}
-		res.json(data);
+		res.json(err ? err : data);
 	});
 });
 
 /* POST /customer */
 router.post('/', function(req, res, next) {
 	Customer.create(req.body, function (err, data) {
-		if (err){
-			return next(err);
-		}
-		res.json(data);
+		res.json(err ? err : data);
 	});
 });
 
 /* GET /customer/id */
 router.get('/:id', function(req, res, next) {
-	Customer.findById(req.params.id, function (err, post) {
-		if (err){
-			return next(err);
-		}
-		res.json(post);
+	Customer.findById(req.params.id, function (err, data) {
+		res.json(err ? err : data);
 	});
 });
 
 /* PUT /customer/:id */
 router.put('/:id', function(req, res, next) {
-	Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-		if (err){
-			return next(err);
-		}
-		res.json(post);
+	Todo.findByIdAndUpdate(req.params.id, req.body, function (err, data) {
+		res.json(err ? err : data);
 	});
 });
 
 /* DELETE /customer/:id */
 router.delete('/:id', function(req, res, next) {
-	Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-		if (err){
-			return next(err);
-		} 
-		res.json(post);
+	Todo.findByIdAndRemove(req.params.id, req.body, function (err, data) {
+		res.json(err ? err : data);
 	});
 });
 
