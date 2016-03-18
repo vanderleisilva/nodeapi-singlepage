a7e0a4c2e318309b1efb849a20e1d28ebadd0015
Vanderlei Silva <vanderleisilva@santos.sp.gov.br>
2016-03-18 17:15:23 -0300

    Change on main script to allow cross domain request

--- app/app.js
+++ app/app.js
@@ -19,11 +19,19 @@ function run(){
 	// parse application/json
 	app.use(bodyParser.json());
 
+	// allow cross domain
+	app.use(function(req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+		next();
+	});
+
 	//routing files
 	app.use('/customers', require('./route/CustomerRoute'));
 
 	//start listening over port 3000
-	app.listen(3000, function () {
+	app.listen(3000, '127.0.0.1', function () {
 		console.log('App listening on port 3000!');
 	});
 }
